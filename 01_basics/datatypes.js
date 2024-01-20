1363656f39fd5c61fda54e3a79aaf7c8dcfb798b
avisheknayak2058 <144879621+avisheknayak2058@users.noreply.github.com>
2024-01-20 20:20:06 +0000

    datatypes

---
+++ 01_basics/datatypes.js
@@ -0,0 +1,6 @@
+'use strict';
+
+let age = 12
+
+console.log(3+3); // we are using node js and not browser
+console.log(typeof undefined);
---
+++ 01_basics/variables.js
@@ -0,0 +1,8 @@
+const accountId = 432435
+let accountEmail = 'nayak.avishek@gmail.com'
+var accountPwd = '12345'
+accountCity = 'Jaipur'
+let accountState
+
+console.log(accountId);
+console.table  ([accountEmail, accountPwd, accountCity, accountState])
